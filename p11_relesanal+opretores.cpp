9911f47f971720c095874d747ccb75f5fe1bf88d
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:05:12 +0530

    //relational operator check

---
+++ p11_relesanal+opretores.cpp
@@ -0,0 +1,21 @@
+//relational operator check
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int a=10,b=9;
+
+	cout<<"\n a>b ="<<(a>b);
+	cout<<"\n a>=b ="<<(a>=b);
+
+	cout<<"\n a<b ="<<(a<b);
+	cout<<"\n a<=b ="<<(a<=b);
+
+	cout<<"\n a==b ="<<(a==b);
+	cout<<"\n a!=b ="<<(a!=b);
+
+return 0;
+}
