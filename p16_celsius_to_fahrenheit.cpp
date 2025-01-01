f5a0a9a00030fbbc95b9eafd65b8ec0bc349feb9
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:08:13 +0530

    //celsius to fahrenheit

---
+++ p16_celsius_to_fahrenheit.cpp
@@ -0,0 +1,19 @@
+//celsius to fahrenheit
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	float cel,fa;
+
+	cout<<"enter the celsius";
+	 cin>>cel;
+
+	fa=32+(cel*9/5);
+
+	cout<<"\n fahrenheit is :"<<fa;
+
+	return 0;
+}
