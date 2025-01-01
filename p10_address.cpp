e474a0a9e0e51d25d41dbaa628553679c8b20adc
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:04:43 +0530

    // user input

---
+++ p10_address.cpp
@@ -0,0 +1,27 @@
+// user input
+#include <iostream>
+using namespace std;
+int main()
+{
+ 	int a,b;
+	cout<<"entre a :";
+	cin>>a;
+	cout<<" enter the b:";
+    cin>>b;
+
+
+
+	cout<<"\n values :a:  \t b:"<<a<<b;
+
+	cout<<"\n sizeof int " <<sizeof(int);
+	cout<<"\n sizeof float " <<sizeof(float);
+	cout<<"\n sizeof char " <<sizeof(char);
+
+	cout<<"\n sizeof double" <<sizeof(double);
+	cout<<"\n sizeof long double %" <<sizeof(long double);
+
+
+
+
+	return 0;
+}
