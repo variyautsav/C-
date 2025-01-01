72c086451f96fa943575bd2bbe93a0e5fe535755
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:06:58 +0530

    //convert the feet into inches

---
+++ p14_feet_into_inches.cpp
@@ -0,0 +1,19 @@
+//convert the feet into inches
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int feet,inch;
+
+	cout<<"enter the feet";
+	cin>>feet;
+
+	inch=feet*12;
+
+	cout<<"\n feet of inche is:= "<<inch;
+
+	return 0;
+}
