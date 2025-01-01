830facdeccedf3ebe0c0fd134d41988cc7194284
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:03:19 +0530

    // area of circle

---
+++ p7_area_of_circal.cpp
@@ -0,0 +1,13 @@
+// area of circle
+#include <iostream>
+using namespace std;
+int main ()
+{
+ 	float pi=3.17,circle;
+	int r;
+	cout<<"enter the r";
+	cin>>r;
+	circle=pi*r*r;
+	cout<<"\n area of circle"<< circle;
+  return 0;
+}
