861cd2f3f4abb5e270254d3474f41bf713e7d763
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:00:50 +0530

    user input

---
+++ p3_user_input.cpp
@@ -0,0 +1,29 @@
+// cin >> user input using cin cin not a function it is standerd libray in iostream->standerd->cout/cin
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a=10,b=20;  // initiliation
+    // int c;          // declaration
+    // cout<<"\n a:"<<a;
+    // cout<<"\n b:"<<b;
+    // cout<<"\n a+b:"<<a+b;
+
+    //userinput
+    int a,b,c;
+    // cout<<"enter a:";
+    // cin>>a;
+    // cout<<"enter b:";
+    // cin>>b;
+    // cout<<"\n a:"<<a;
+    // cout<<"\n b:"<<b;
+    // cout<<"\n a+b:"<<a+b;
+
+    cout<<"enter a&b:";
+    cin>>a>>b;
+    cout<<"\n a:"<<a;
+    cout<<"\n b:"<<b;
+    cout<<"\n a+b:"<<a+b;
+
+return 0;
+}
