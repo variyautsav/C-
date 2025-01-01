4da7565cb34458ac5a0e7d0a6a457b3f73dccb41
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:07:22 +0530

    //convert the   inches to cm

---
+++ p15_inche_into_cm.cpp
@@ -0,0 +1,20 @@
+//convert the   inches to cm
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int inch;
+	float cm;
+
+	cout<<"enter the inch";
+	cin>>inch;
+
+	cm=inch*2.54;
+
+     cout<<"  inche to cm is:"<<cm<<endl;
+
+	return 0;
+}
