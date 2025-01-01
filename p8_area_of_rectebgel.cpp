03284f167f41fcd46b4cc97cfa83f662678bbea7
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:03:55 +0530

    // area of rectangle

---
+++ p8_area_of_rectebgel.cpp
@@ -0,0 +1,14 @@
+// area of rectangle
+#include <iostream>
+using namespace std;
+ int main()
+{
+	int l=100,b=69,area;
+	cout<<"enter the l";
+	cin>>l;
+    cout<< "enter the b";
+    cin>>b;
+ 	area=l*b;
+	cout<<" \n area of rectangle "<< area;
+     return 0;
+}
