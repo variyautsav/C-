7d28e8c70cac4375f953c273054b03beeaa2aed0
variya utsav <utsavvariya02@gmail.com>
2025-01-01 15:57:56 +0530

    // endl and \n

---
+++ p2_endl_andn.cpp
@@ -0,0 +1,13 @@
+// endl and \n
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=20;
+    // cout<<"\n a:"<<a;
+    // cout<<"\n b:"<<b;
+    // cout<<"\n a+b:"<<a+b;
+    cout<<a<<endl;
+    cout<<b;
+    cout<<"\n"<<a+b;
+}
