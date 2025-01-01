61348143a26435f33f223da9b2f883dc9d118bf4
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:02:49 +0530

    // simple interest

---
+++ p6_simpel_intrest.cpp
@@ -0,0 +1,18 @@
+// simple interest
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ 	int p=100,r=2,n=12,amount;
+      cout<<"entre the parice ";
+     cin>>p;
+      cout<<"\n entre the rate ";
+	cin>>r;
+     cout<<"enter the month ";
+	cin>>n;
+
+	amount=(p*r*n)/100;
+	cout<<"\n total amount with inteset "<<amount;
+ return 0;
+}
