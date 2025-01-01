2489016069210dd60fceaceb744b0afab4d628e7
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:04:20 +0530

    // area of teingle

---
+++ p9_area_of_tringel.cpp
@@ -0,0 +1,15 @@
+// area of teingle
+
+#include <iostream>
+using namespace std;
+ int main()
+{
+	int b=89,h=30,area;
+   cout<<"enter the b";
+    cin>>b;
+	cout<<"enter the h";
+	cin>>h;
+	area=0.5*b*h;
+	cout<<"\n area of teingle ", area;
+return 0;
+}
