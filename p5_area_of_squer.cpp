451145db1fd9f2abe381603c66cf7e5ad88521a0
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:02:21 +0530

    // area of square

---
+++ p5_area_of_squer.cpp
@@ -0,0 +1,14 @@
+// area of square
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	 int l,area;
+	cout<<" entre the lanth ";
+	cin>>l;
+
+	area=l*l;
+	cout<<"\n area of square "<<area;
+return 0;
+}
