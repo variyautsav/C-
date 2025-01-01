c27851495fc36759b2388fec7e7a897d363ecb97
variya utsav <utsavvariya02@gmail.com>
2025-01-01 16:06:27 +0530

    //doller to rupee

---
+++ p13_doller_to_rupee.cpp
@@ -0,0 +1,19 @@
+//doller to rupee
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+	float d,r;
+
+	cout<<"enter the doller";
+	cin>>d;
+
+	r=d*83;
+	cout<<"\n ruppes of doller :"<<r;
+
+	return 0;
+}
