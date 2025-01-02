b134bbf5dc2dd0b6f9b8003fe77b501242e04d6d
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:05:36 +0530

    // nagetive positive ,  zero

---
+++ p23_nagitive_positive_zero.cpp
@@ -0,0 +1,24 @@
+// nagetive positive ,  zero
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the a:";
+	cin>>a;
+
+    if(a>=0)
+	{
+		if(a==0)
+			{
+				cout<<" number is zero "<<a;
+			}
+		cout<<"number is positive "<< a;
+       }
+
+     else
+		{
+		cout<<"number is nagetive "<< a;
+		}
+	return 0;
+}
