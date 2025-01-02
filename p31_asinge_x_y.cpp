dadd23dd54d56e0fca0a0b42fc0d49f84d7e5d2a
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:08:43 +0530

    // asinge the x value or y value

---
+++ p31_asinge_x_y.cpp
@@ -0,0 +1,23 @@
+// asinge the x value or y value
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"enter the x :";
+	cin>>x;
+	cout<<"enter the y:";
+	cin>>y;
+
+	if(x<2000 || x>3000)
+		{
+		    cout<<" x";
+		}
+
+       if(y>=100 && y<=500)
+		{
+			cout<<" y";
+		}
+
+	return 0;
+}
