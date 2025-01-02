2099423879422b24e6e0e09b5a1d4e4e1b5a1ef1
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:04:56 +0530

    //number is devide by 5 or not

---
+++ p21_divisible_5.cpp
@@ -0,0 +1,27 @@
+//number is devide by 5 or not
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+	int a;
+
+
+	cout<<"enter the a";
+	cin>>a;
+
+
+
+	if(a%5==0)
+	{
+		cout<<"a"<<a<<" is devide by 5";
+	}
+	else
+	{
+		cout<<"a"<<a<<" is not  devide by 5";
+	}
+	return 0;
+}
