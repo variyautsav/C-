49f95360aba1cfb3cb063b5ce0d5b58d44c8a7da
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:04:37 +0530

    //number is positive or nagitive

---
+++ p20_positive_or_nagitiv.cpp
@@ -0,0 +1,26 @@
+//number is positive or nagitive
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int a;
+
+
+	cout<<"enter the a";
+	cin>>a;
+
+
+
+	if(a>0)
+	{
+		cout<<"a"<<a<<" is positive number";
+	}
+	else
+	{
+		cout<<"a"<<a<<" is nagitive number";
+	}
+	return 0;
+}
