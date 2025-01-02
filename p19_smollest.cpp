30b60bbbc271d21e7282f4c65b1ed71efe7e2112
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:04:18 +0530

    //number check

---
+++ p19_smollest.cpp
@@ -0,0 +1,27 @@
+
+//number check
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+
+	cout<<"enter the a:";
+	cin>>a;
+
+	cout<<"enter the b:";
+	cin>>b;
+
+	if(a>b)
+	{
+      cout<<"b "<<b<< "smoll than a"<<a;
+	}
+	else
+	{
+
+        cout<<"a "<<a<<" smoll than b"<<b;
+	}
+
+	return 0;
+}
