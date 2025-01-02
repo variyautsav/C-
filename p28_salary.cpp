72ceeb0cfc49fbbd1d3ee2f50c7aa7b7930b7192
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:07:37 +0530

    salary

---
+++ p28_salary.cpp
@@ -0,0 +1,45 @@
+// salary
+#include<iostream>
+using namespace std;
+int main()
+{
+ float salary,hra,da,total;
+
+ cout<<"enter the salary ";
+ cin>>salary;
+
+ if(salary<=5000)
+	{
+		hra=(salary*8)/100;
+		da=(salary*20)/100;
+		total =salary+hra+da;
+		cout<< "total salary "<<total;
+	}
+else  if(salary>5000 && salary<10000)
+	{
+		hra=(salary*12)/100;
+		da=(salary*30)/100;
+		total =salary+hra+da;
+		cout<<" total salary "<<total;
+	}
+ else if(salary>10000 && salary<15000)
+	        {
+		hra=(salary*15)/100;
+		da=(salary*40)/100;
+		total =salary+hra+da;
+		cout<<" total salary "<<total;
+
+	         }
+else
+  {
+
+	hra=(salary*20)/100;
+	da=(salary*50)/100;
+	total =salary+hra+da;
+	cout<<" total salary"<<total;
+
+   }
+
+
+return 0;
+}
