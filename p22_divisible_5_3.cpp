db0b248b3e92ffff7a6f96b34824519db015de0d
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:05:15 +0530

    // divisible by 5 or 3

---
+++ p22_divisible_5_3.cpp
@@ -0,0 +1,21 @@
+// divisible by 5 or 3
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+
+	cout<<"enter the a:";
+	cin>>a;
+
+	if(a%15==0)
+	{
+    cout<<" a"<< a<<" is divisible ";
+	}
+	else
+	{
+	cout<<" \n a"<<a<<" is not divisible ";
+	}
+
+	return 0;
+}
