62d37f67b23bdef495ea4ef7ac692a7984827789
variya utsav <utsavvariya02@gmail.com>
2025-01-02 16:02:41 +0530

    //number check

---
+++ p18_largest.cpp
@@ -0,0 +1,26 @@
+
+//number check
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+
+	cout<<"enter the a:";
+	cin>>a;
+
+	cout<<"enter the b:";
+	cin>>b;
+
+	if(a>b)
+	{
+		cout<<"a "<<a<<" grater than b"<<b;
+	}
+	else
+	{
+		cout<<"b "<<b<< "grater than a"<<a;
+	}
+
+	return 0;
+}
