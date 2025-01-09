b2bdf816795e001d65c9bb2d12460f28696ffc8a
variya utsav <utsavvariya02@gmail.com>
2025-01-09 16:06:49 +0530

    44_1

---
+++ p44_1_n.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+  cout<<"entre the n";
+  cin>>n;
+    for(i=0;i<=n;i++)
+    {
+        cout<<" \n"<<i;
+    }
+    return 0;
+
+}
