e8872b9a80cce682d3604db8ae98d9b1ac47e7aa
variya utsav <utsavvariya02@gmail.com>
2025-01-16 16:17:06 +0530

    odd

---
+++ p46_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<=50;i++)
+    {
+        if(i%2==1)
+        {
+            cout<<" \n "<<i;
+        }
+    }
+    return 0;
+}
