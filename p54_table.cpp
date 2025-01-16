83696cf009cf47ec8075e4ceb0d96b9022e3663f
variya utsav <utsavvariya02@gmail.com>
2025-01-16 16:21:07 +0530

    table

---
+++ p54_table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,multi;
+    printf(" enter the table :");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        cout<<n<< "*" <<i<<" ="<< n*i<<endl;
+
+    }
+    return 0;
+}
