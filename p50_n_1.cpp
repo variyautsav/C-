4a9e5464a7d328b80af006bf1668c207463d1c6e
variya utsav <utsavvariya02@gmail.com>
2025-01-16 16:18:56 +0530

    n-1

---
+++ p50_n_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<" enter the  n :";
+    cin>>n;
+    for(i=n;i>=1;i--)
+    {
+        cout<<i;
+
+    }
+    return 0;
+
+}
+
