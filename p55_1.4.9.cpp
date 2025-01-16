03e3e9338f6269628e632862aeec1982ead46a1c
variya utsav <utsavvariya02@gmail.com>
2025-01-16 16:21:34 +0530

    1.4.9

---
+++ p55_1.4.9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,total;
+    cout<<"enter the n :";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+          total=i*i;
+        cout<<" \n"<<total;
+
+    }
+    return 0;
+
+}
