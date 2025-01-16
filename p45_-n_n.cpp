e1764f28f808d8043f9d3f7892e6d49e118e8b1c
variya utsav <utsavvariya02@gmail.com>
2025-01-16 16:16:39 +0530

    -n_n

---
+++ p45_-n_n.cpp
@@ -0,0 +1,17 @@
+    #include<iostream>
+using namespace std;
+
+int main()
+    {
+        int n,i;
+        cout<<"entre the n:";
+        cin>>n;
+        for(i=-n;i<=n;i++)
+        {
+            cout<<"\n "<<i;
+        }
+        return 0;
+
+
+    }
+
