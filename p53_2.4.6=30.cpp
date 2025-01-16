e82048a8d3e54172ad3e11c0f0df266372007e0c
variya utsav <utsavvariya02@gmail.com>
2025-01-16 16:20:40 +0530

    2.4.6=30

---
+++ p53_2.4.6=30.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,total=0;
+    for(i=0;i<=10;i+=2)
+    {
+        total=total+i;
+    }
+    cout<<"total= "<<total;
+    return 0;
+}
