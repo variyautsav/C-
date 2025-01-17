1b87f284f6e0527c7498873aeacf2a956396800d
variya utsav <utsavvariya02@gmail.com>
2025-01-17 15:40:12 +0530

    1.2.4.8

---
+++ p57_1.2.4.8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,total=1;
+    cout<<"enter the n :";
+    cin>>n;
+    for(i=1;i<=n;i*=2)
+    {
+
+        cout<<"\n"<<i;
+
+    }
+    return 0;
+
+}
