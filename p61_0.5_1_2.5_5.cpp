a1ab1c0a4ad8b0453eb671b47894d2bfe30fdb93
variya utsav <utsavvariya02@gmail.com>
2025-01-20 16:43:19 +0530

    _0.5_1_2.5_5

---
+++ p61_0.5_1_2.5_5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     float i,n=0.5;
+
+     cout<<n;
+     for(i=0.5;i<=10;i++)
+     {
+      n=n+i;
+      cout<<n;
+     }
+
+     return 0;
+}
