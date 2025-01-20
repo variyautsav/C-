c71c694eae234254d9db4777cea5c9d2fe4d5bdb
variya utsav <utsavvariya02@gmail.com>
2025-01-20 16:44:53 +0530

    1111_2222_3333_444_5555_

---
+++ p64_1111_2222_3333_444_5555_.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            cout<<i<< "\t";
+        }
+
+        cout<<"\n";
+    }
+    return 0;
+}
+/*
+1111
+2222
+3333
+4444
+*/
