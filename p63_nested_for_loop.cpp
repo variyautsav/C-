963c81ec5ee272bb162973f46389292c8176e9f5
variya utsav <utsavvariya02@gmail.com>
2025-01-20 16:44:17 +0530

    nested_for_loop

---
+++ p63_nested_for_loop.cpp
@@ -0,0 +1,23 @@
+/*
+  ***
+  ***
+  ***
+  ***
+
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)//row
+    {
+        for(j=1;j<=3;j++)// colme
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
