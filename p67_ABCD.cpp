150a8e3d0e567dcb1643037e74ea89c407cf2aff
variya utsav <utsavvariya02@gmail.com>
2025-01-24 16:04:24 +0530

    AAAA BBBB CCCC DDDD

---
+++ p67_ABCD.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=65;i<69;i++)
+    {
+        for(j=65;j<69;j++)
+        {
+            cout<<char(i)<<"\t";
+        }
+
+        cout<<"\n";
+    }
+    return 0;
+}
+/*
+AAAA
+BBBB
+CCCC
+DDDD
+*/
