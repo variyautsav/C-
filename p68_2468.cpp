0a8e4893c0b013fab891d12e19a23eec15164932
variya utsav <utsavvariya02@gmail.com>
2025-01-24 16:05:04 +0530

    22222 4444 6666 8888 1001

---
+++ p68_2468.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=2;i<=10;i+=2)
+    {
+        for(j=2;j<=10;j+=2)
+        {
+            cout<< i<<"\t";
+        }
+
+     cout<<"\n";
+    }
+    return 0;
+}
+/*
+22222
+4444
+6666
+8888
+1001
+*/
