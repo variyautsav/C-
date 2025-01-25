63495a571581be02571e17439941a1d8ed41eb0d
variya utsav <utsavvariya02@gmail.com>
2025-01-25 14:18:31 +0530

    petent

---
+++ p74_petent.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+     for(i=1;i<=5;i++)
+     {
+        for(j=1;j<=i;j++)
+        {
+        cout<<j<<"\t";
+        }
+        cout<<"\n";
+     }
+     return 0;
+}
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
