0a4a3b46730d04899ca841df6872c7f1c7a3f089
variya utsav <utsavvariya02@gmail.com>
2025-02-04 14:53:08 +0530

    // while a.....z;

---
+++ p94_while_a...z.cpp
@@ -0,0 +1,14 @@
+// while a.....z;
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    i=65;
+    while(i<=90)
+    {
+        cout<<"\t "<<char(i++);
+
+    }
+    return 0;
+}
